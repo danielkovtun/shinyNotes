4db16a4d38fccfa789f5ecffc820c08d859611d8
WINDOWS-LQSQ5O0\danny <36872126+danielkovtun@users.noreply.github.com>
2020-01-08 10:58:56 -0500

    prepare package for CRAN submission

--- R/data.R
+++ R/data.R
@@ -1,14 +1,16 @@
-#' Demo notes for testing shinyNote module.
+#' Demo notes for testing \code{shinynote} module.
 #'
-#' A dataset containing package functions and their titles for the shiny, shinyWidgets and dplyr packages.
-#' Formatted in a structure compatible with the shinyNote's shiny module.
+#' A dataset containing package functions and their titles for the \code{shiny}, \code{shinyWidgets} and \code{dplyr} packages.
+#' Formatted in a structure compatible with the \code{shinyNotes::shinynotes} module.
 #'
-#' @format A tibble with 274 rows and 3 variables:
+#' @format A \code{tibble} with 274 rows and 3 variables:
 #' \describe{
 #'   \item{package}{package title, character class}
 #'   \item{category}{function name, character class}
 #'   \item{update}{function title, character class}
 #'   ...
 #' }
-#' @source \href{https://cran.r-project.org/web/packages/available_packages_by_name.html}{CRAN Package Help Pages}
+#' @source \href{https://CRAN.R-project.org/package=shiny}{shiny help pages}
+#' @source \href{https://CRAN.R-project.org/package=shinyWidgets}{shinyWidgets help pages}
+#' @source \href{https://CRAN.R-project.org/package=dplyr}{dplyr help pages}
 "demo_notes"
